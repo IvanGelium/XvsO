4e19ba8299bde0509c58d1270dcda899e9555524
Leonid <l.shkurchenko@dcrt.it>
2025-02-11 18:47:28 +0300

    some logic

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 
 
+const zoneOfControl = document.querySelector(".zoneOfControl");
+
+
+// const round = (function(){
+    
+// })();
+
 //#region Модуль отвечающий за игровой процесс 
 
-const xVSo = (function (doc){
-    const battleField = doc.querySelector(".battleField");
-    battleField.addEventListener("click",(e) => clickLocation(e))
-
-    const zoneOfControl = doc.querySelector(".zoneOfControl");
-    zoneOfControl.addEventListener("click", (e)=> {console.log(e)})
-
-    function clickLocation (e) {
-        x = Math.floor(e.offsetX/300)
-        y = Math.floor(e.offsetY/300)
-        boardUpdate(x,y);
-        console.table(board);
-        return {y,x};
-    }
+const boardG = (function (doc){
 
     function boardCreation () {
         let brd = [];
@@ -32,11 +26,11 @@ const xVSo = (function (doc){
 
     function boardUpdate (x,y,value = 0) {
         board[y][x] = value;
+        console.table(boardG.board);
+        game.round();
     }
 
-    let whosTurn = Math.round(Math.random());
-
-    return {board, whosTurn,boardUpdate, battleField}
+    return {board,boardUpdate}
 
 
 
@@ -45,18 +39,59 @@ const xVSo = (function (doc){
 //#endregion
 //#region Модуль Игрок
 
-const user = (function (){
+const user = (function (doc){
     let value = `X`;
     let name = `Player`;
+    let pos = {x:0,y:0};
+    let isTurn = -1  ;
+    const turnRele = function () {
+        console.log (`Ходит ${name}`);
+        isTurn *= -1;
+        if (isTurn === 1) {
+            battleField.addEventListener("click", clickLocation);
+            console.log ("Player control ON")
+        } 
+        
+    }
+    const battleField = doc.querySelector(".battleField");
+    function clickLocation (e) {
+        pos.x = Math.floor(e.offsetX/300)
+        pos.y = Math.floor(e.offsetY/300)
+        console.log (`Игрок выбрал координаты ${pos.x+1} и ${pos.y+1}` );
+        boardG.boardUpdate(pos.x,pos.y,value  )
+    }
 
-})();
+    return {
+        getName: () => name,
+        getValue: () => value,
+        getPos: () => pos,
+        getIsTurn: () => isTurn,
+        turnRele
+    } 
+})(document);
 
 //#endregion
 //#region Модуль Бот
 
-const bot = (function (){
+const bots = (function (){
+    let value = `O`;
+    let name = `Bot`;
+    let pos = {x:0,y:0};
 
+    const botPlace = () => {
+        //placeholder
+        console.log (`Ходит ${name}`);
+        pos.x = Math.floor(Math.random() * 3)
+        pos.y = Math.floor(Math.random() * 3)
+        boardG.boardUpdate(pos.x,pos.y,value)
+    }
     
+    return {
+        getName: () => name,
+        getValue: () => value,
+        getPos: () => pos,
+        botPlace
+    }
 
 })();
 
@@ -65,6 +100,37 @@ const bot = (function (){
 //#region Модуль Раундов
 
 const turnSys = (function (){
+    zoneOfControl.removeEventListener("click", () => game.round());
+    let isWin;
+    let whosTurn = Math.round(Math.random());
+    let counter = 0;
+    console.log(`Первый ход за ${whosTurn}`)
+    const round = () => {
+        console.log (`Начинается раунд ${counter}` );
+        counter++;
+        flipTurn();
+        console.log (whosTurn);
+        if (whosTurn === 1) {
+            player.turnRele();
+        }
+
+        if (whosTurn === 0) {
+            bot.botPlace();
+        }
+
+    }
+
+    function flipTurn () {
+        if (whosTurn === 0) { whosTurn = 1;return}
+        if (whosTurn === 1) { whosTurn = 0;return}
+    }
+
+    function winCheck () { 
+            
+    }
+
+
+    return {round}
 
 })();
 
@@ -79,3 +145,9 @@ const screenRefresher = (function (){
 })();
 
 //#endregion
+
+const player = user;
+const bot = bots;
+const game = turnSys;
+
+zoneOfControl.addEventListener("click", () => game.round());
